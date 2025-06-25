1080cb03ea36889b909731e14148307baac32922
Mathilde1420 <kassemathilde@gmail.com>
2025-06-25 10:54:34 +0000

    mise a jours

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 "use client";
 
 import { useState, useEffect } from "react";
